2210d612deb4dbc59eddb5ea7687f4f3f3a246d4
Almas <aalmasabilov@gmail.com>
2026-03-15 22:57:58 +0500

    add basic attack abstraction

--- src/com/narxoz/rpg/decorator/BasicAttack.java
+++ src/com/narxoz/rpg/decorator/BasicAttack.java
@@ -1,29 +1,39 @@
 package com.narxoz.rpg.decorator;
 
+import java.util.Objects;
+
 public class BasicAttack implements AttackAction {
     private final String actionName;
     private final int baseDamage;
 
     public BasicAttack(String actionName, int baseDamage) {
-        this.actionName = actionName;
+        this.actionName = requireNonBlank(actionName, "actionName");
+        if(baseDamage <= 0){
+            throw new IllegalArgumentException("baseDamage must be > 0");
+        }
         this.baseDamage = baseDamage;
     }
 
     @Override
     public String getActionName() {
-        // TODO: Return the base name of the action.
         return actionName;
     }
 
     @Override
     public int getDamage() {
-        // TODO: Return the base damage before decorators modify it.
         return baseDamage;
     }
 
     @Override
     public String getEffectSummary() {
-        // TODO: Decide how a plain attack should describe its effects.
-        return "TODO";
+        return "Base attack";
+    }
+    private static String requireNonBlank(String value, String paramName){
+        Objects.requireNonNull(value, paramName + " mustn't be null");
+        String trimmed = value.trim();
+        if(trimmed.isBlank()){
+            throw new IllegalArgumentException(paramName + " mustn't be blank");
+        }
+        return trimmed;
     }
 }
