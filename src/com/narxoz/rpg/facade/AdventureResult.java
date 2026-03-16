e1da805e597e0d9034b876e0bf450d05ede0ed82
Almas <aalmasabilov@gmail.com>
2026-03-16 17:32:15 +0500

    implement adventure result class

--- src/com/narxoz/rpg/facade/AdventureResult.java
+++ src/com/narxoz/rpg/facade/AdventureResult.java
@@ -3,11 +3,19 @@ package com.narxoz.rpg.facade;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class AdventureResult {
     private String winner;
     private int rounds;
     private String reward;
+    private int heroStartHealth;
+    private int heroEndHealth;
+    private int bossStartHealth;
+    private int bossEndHealth;
+    private String actionName;
+    private int actionDamage;
+
     private final List<String> log = new ArrayList<>();
 
     public String getWinner() {
@@ -34,8 +42,60 @@ public class AdventureResult {
         this.reward = reward;
     }
 
+    public int getHeroStartHealth() {
+        return heroStartHealth;
+    }
+
+    public void setHeroStartHealth(int heroStartHealth) {
+        this.heroStartHealth = heroStartHealth;
+    }
+
+    public int getHeroEndHealth() {
+        return heroEndHealth;
+    }
+
+    public void setHeroEndHealth(int heroEndHealth) {
+        this.heroEndHealth = heroEndHealth;
+    }
+
+    public int getBossStartHealth() {
+        return bossStartHealth;
+    }
+
+    public void setBossStartHealth(int bossStartHealth) {
+        this.bossStartHealth = bossStartHealth;
+    }
+
+    public int getBossEndHealth() {
+        return bossEndHealth;
+    }
+
+    public void setBossEndHealth(int bossEndHealth) {
+        this.bossEndHealth = bossEndHealth;
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public void setActionName(String actionName) {
+        this.actionName = actionName;
+    }
+
+    public int getActionDamage() {
+        return actionDamage;
+    }
+
+    public void setActionDamage(int actionDamage) {
+        this.actionDamage = actionDamage;
+    }
+
     public void addLine(String line) {
-        log.add(line);
+        log.add(Objects.requireNonNull(line, "line mustn't be null"));
+    }
+
+    public void prependLine(String line) {
+        log.add(0, Objects.requireNonNull(line, "line must not be null"));
     }
 
     public List<String> getLog() {
